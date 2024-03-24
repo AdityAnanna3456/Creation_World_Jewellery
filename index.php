cad390ed4a5d1e874fad2822356800d79bd92174
AdityAnanna <adityananna14@gmail.com>
2024-03-24 21:16:46 +0600

    initial commit

---
+++ index.php
@@ -0,0 +1,251 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Creation World Jewellery</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+    <div>
+        <nav>
+
+
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="login.php">Admin</a></li>
+                <li><a href="rate.php">Rate</a></li>
+            </ul>
+
+        </nav>
+        <!-- <img src="designs/menu.png" class="menu-icon"> -->
+    </div>
+
+    <!-- </div> -->
+    <!-- navbar section ends here -->
+
+
+
+    <!-- Home section begins here -->
+    <div class="row">
+        <div class="cols-2">
+            <h1>Give your looks<br>A New Style</h1>
+            <p>"Make jewellery contact before eyes contact"</p>
+
+        </div>
+        <div class="cols-2">
+            <img src="designs/design3.jpg" alt="design">
+        </div>
+    </div>
+    <!-- </div> -->
+
+    <section class="row1">
+        <div class="cols-2">
+            <img src="designs/design4.jpg" alt="design">
+        </div>
+        <div class="cols-2">
+            <h1>Jewellery is like an fashion.<br></h1>
+            <p>"Always changes from time to time"</p>
+            <!-- <a href="" class="btn">See more &#8594;</a> -->
+        </div>
+    </section>
+
+    <!-- Home section ends here -->
+    <!-- About section begins here -->
+    <section class="about" id="about">
+        <h2>About</h2>
+        <div class="row">
+            <div class="cols-2">
+                <p>Creation world Jewellers is one of the cost friendly jewellery shop. It gives the quality services of
+                    ordering the jewellery items.It had got a experience and well trained employees.</p>
+
+            </div>
+
+        </div>
+
+    </section>
+    <!-- About section ends here -->
+
+    <!-- Gallery Section begins here -->
+    <section class="gallery" id="gallery">
+        <h2>Gallery</h2>
+        <div class="row">
+            <div class="cols-2">
+                <h2 class="details">Necklace</h2>
+                <ul type="none">
+                    <li>Caret:24</li><br>
+                    <li>Material:Gold</li><br>
+                    <li>Weight:20gm</li><br>
+                    <li>Code:#01</li><br>
+                    <!-- <li>Price:NRS.15000|-</li> -->
+                </ul>
+                <a href="form.php" class="btn">Order Now &#8594;</a>
+
+            </div>
+
+            <div class="cols-2">
+                <img src="designs/necklace1.jpg" alt="design">
+            </div>
+
+
+        </div>
+        <!-- </div> -->
+
+
+
+
+        <div class="row1">
+            <div class="cols-2">
+                <img src="designs/rings1.jpg" alt="design">
+            </div>
+            <div class="cols-2">
+                <h2 class="details">Ring</h2>
+                <ul type="none">
+                    <li>Caret:23</li><br>
+                    <li>Material:Gold</li><br>
+                    <li>Weight:20gm</li><br>
+                    <li>Code:#02</li><br>
+                    <!-- <li>Price:NRS.15000|-</li> -->
+                </ul>
+                <a href="form.php" class="btn">Order Now &#8594;</a>
+
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="cols-2">
+                <h2 class="details">Anklet</h2>
+                <ul type="none">
+                    <li>Caret:24</li><br>
+                    <li>Material:Silver</li><br>
+                    <li>Weight:20gm</li><br>
+                    <li>Code:#03</li><br>
+                    <!-- <li>Price:NRS.1500|-</li> -->
+
+                </ul>
+                <a href="form.php" class="btn">Order Now &#8594;</a>
+
+            </div>
+
+            <div class="cols-2">
+                <img src="designs/anklet1.jpg" alt="design">
+            </div>
+
+
+        </div>
+
+
+
+
+        <div class="row">
+            <div class="cols-2">
+                <h2 class="details">Bangle</h2>
+                <ul type="none">
+                    <li>Caret:24</li><br>
+                    <li>Material:Gold</li><br>
+                    <li>Weight:20gm</li><br>
+                    <li>Code:#07</li><br>
+                    <!-- <li>Price:NRS.15000|-</li> -->
+                </ul>
+                <a href="form.php" class="btn">Order Now &#8594;</a>
+
+            </div>
+
+            <div class="cols-2">
+                <img src="designs/bangles1.jpg" alt="design">
+            </div>
+        </div>
+
+        <div class="row1">
+            <div class="cols-2">
+                <img src="designs/bracelets1.jpg" alt="design">
+            </div>
+            <div class="cols-2">
+                <h2 class="details">Bracelet</h2>
+                <ul type="none">
+                    <li>Caret:22</li><br>
+                    <li>Material:Gold</li><br>
+                    <li>Weight:22gm</li><br>
+                    <li>Code:#08</li><br>
+                    <!-- <li>Price:NRS.20000|-</li> -->
+                </ul>
+                <a href="form.php" class="btn">Order Now &#8594;</a>
+
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="cols-2">
+                <h2 class="details">Ear Rings</h2>
+                <ul type="none">
+                    <li>Caret:24</li><br>
+                    <li>Material:Gold</li><br>
+                    <li>Weight:20gm</li><br>
+                    <li>Code:#09</li><br>
+                    <!-- <li>Price:NRS.15000|-</li> -->
+                </ul>
+                <a href="form.php" class="btn">Order Now &#8594;</a>
+
+            </div>
+
+            <div class="cols-2">
+                <img src="designs/ear rings1.jpg" alt="design">
+            </div>
+        </div>
+
+    </section>
+
+    <!-- Gallery section ends here -->
+
+    <!-- Contact section begins here -->
+    <section class="contact" id="contact">
+        <h2>Contact Us</h2>
+        <h3>Dhaka Dhaka Bangladesh<br>
+            Tel:+02456754<br>
+            Fax:+025674567<br>
+            Email:creation world jewellery@gmail.com<br>
+        </h3>
+
+    </section>
+    <!-- Contact section ends here -->
+    <!-- Footer section begins here -->
+    <div class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="footer-cols-1">
+                    <h3>Download Our App</h3>
+                    <p>Download App for Andriod and ios mobile phone</p>
+                    <div class="app-logo">
+
+                        <img src="designs/app-store.png">
+                        <img src="designs/play-store.png">
+                    </div>
+                </div>
+                <div class="footer-cols-2">
+
+                    <p>Download App for Andriod and ios mobile phone</p>
+                </div>
+                <div class="footer-cols-3">
+                    <h3>Follow Us on</h3>
+                    <!-- Social media section -->
+                    <div class="socialmedia">
+                        <img src="designs/facebook.png"></a>
+                        <img src="designs/instagram.png"> </a>
+                        <img src="designs/twitter.png">
+                        <img src="designs/youtube.jpg">
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <!-- Copright section -->
+            <p class="copyright">© 2024 Creation world jewellery. All rights reserved.</p>
+
+        </div>
+    </div>
+</body>
+
+</html>
